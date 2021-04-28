c42a9afc101aa12427d6c55239ca3d1421d7905f
neurotok <marcin@pingpongcat.com>
2021-04-28 09:29:05 +0200

    local memory

--- main.c
+++ main.c
@@ -1,42 +1,32 @@
+#include <bits/stdint-uintn.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 #include "compute.h"
 #include "instance.h"
 #include "device.h"
 #include "pipeline.h"
 #include "memory.h"
 
- #define STB_IMAGE_WRITE_IMPLEMENTATION
- #include "stb_image_write.h"
-
  #define COLOR_CHANNELS 4
 
-uint32_t InputData[1000];
-float OutputData[1000][1000];
+uint32_t InputData[2][10000];
+uint32_t OutputData[1];
 
 void generate(void)
 {
-    for (uint32_t row = 0; row < 1000; row++)
+    uint32_t maxValue = 0;
+    for (uint32_t i = 0; i < 10000; i++)
     {
-        for (uint32_t column = 0; column < 1000; column++)
-        {
-            float r = (float)row / 500.0 - 1.0;
-            float i = (float)column / 500.0 - 1.0;
+       for (uint32_t j = 0; j < 10000; j++)
+       {
+           uint32_t value = InputData[0][i] * InputData[1][j];
+           
+           if (value > maxValue) maxValue = value;
+       }
 
-            uint32_t cnt = 0;
-            while ((r * r + i * i < 4.0) && (cnt < 63))
-            {
-                float temp = r*r - i * i + 0.17;
-                i = 2 * r * i + 0.57;
-                r = temp;
-                cnt++;
-            }
-            OutputData[row][column] = (cnt << 10) | 0xff000000;
-            
-        }
-        
     }
-    
+    OutputData[0] = maxValue;
 }
 
 int main(int argc, char const *argv[])
@@ -50,7 +40,13 @@ int main(int argc, char const *argv[])
     CreateCommandPool();    
     PrepareCommandBuffer();
 
-    CoppyToInputBuffer(InputData, sizeof(InputData));
+    for (uint32_t i = 0; i < 10000; i++)
+    {
+        InputData[0][i] = rand() % 1000;
+        InputData[1][i] = rand() % 1000;
+    }
+
+    CopyToInputBuffer(InputData, sizeof(InputData));
 
     double time_spent = 0.0;
     clock_t begin = clock();
@@ -59,21 +55,21 @@ int main(int argc, char const *argv[])
 
     clock_t end = clock();
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time of CPU generated fractal: %f s.\n", time_spent);
+    printf("Vectors calulation max result: %d CPU time : %f s.\n", OutputData[0], time_spent);
 
-    stbi_write_bmp("fractactal_cpu.bmp", 1000, 1000, COLOR_CHANNELS, OutputData);    
+    OutputData[0] = 0;
+    CopyFromOutputBuffer(OutputData, sizeof(OutputData));
 
     begin = clock();
 
     Compute();
-    CoppyFromOutputBuffer(OutputData, sizeof(OutputData));
+    CopyFromOutputBuffer(OutputData, sizeof(OutputData));
 
     end = clock();
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time of GPU generated fractal: %f s.\n", time_spent);
+    printf("Vectors calulation max result: %d GPU time : %f s.\n", OutputData[0], time_spent);
 
 
-    stbi_write_bmp("fractactal_gpu.bmp", 1000, 1000, COLOR_CHANNELS, OutputData);    
 
     DestroyPipeline();
     DestroyShaderModule();
--- memory.c
+++ memory.c
@@ -1,141 +1,156 @@
-#include <vulkan/vulkan.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
-#include "memory.h"
+#include "compute.h"
 #include "device.h"
 #include "instance.h"
-#include "compute.h"
+#include "memory.h"
 
 VkBuffer InputBuffer = VK_NULL_HANDLE;
 VkBuffer OutputBuffer = VK_NULL_HANDLE;
 VkDeviceMemory InputBufferMemory = VK_NULL_HANDLE;
 VkDeviceMemory OutputBufferMemory = VK_NULL_HANDLE;
 
-static uint32_t FindMemoryIndexByType(uint32_t allowedTypeMask, VkMemoryPropertyFlags flags)
-{
-    VkPhysicalDeviceMemoryProperties memProperties;
-    vkGetPhysicalDeviceMemoryProperties(PhysicalDevice, &memProperties);
+static uint32_t FindMemoryIndexByType(uint32_t allowedTypeMask,
+                                      VkMemoryPropertyFlags flags) {
+  VkPhysicalDeviceMemoryProperties memProperties;
+  vkGetPhysicalDeviceMemoryProperties(PhysicalDevice, &memProperties);
 
-    uint32_t typeMask = 1;
+  uint32_t typeMask = 1;
 
-    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++, typeMask <<= 1)
-    {
-        if ((allowedTypeMask & typeMask) != 0)
-        {
-            if ((memProperties.memoryTypes[i].propertyFlags & flags) == flags)
-            {
-                return i;
-            }
-        }
+  for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++, typeMask <<= 1) {
+    if ((allowedTypeMask & typeMask) != 0) {
+      if ((memProperties.memoryTypes[i].propertyFlags & flags) == flags) {
+        return i;
+      }
     }
+  }
 
-    printf("Failed to find memory type index\n");
-    return 0;
+  printf("Failed to find memory type index\n");
+  return 0;
 }
 
-VkBuffer CreateBufferAndMemory(size_t size, VkDeviceMemory *defviceMemory)
-{
-    VkBuffer buffer;
-    VkDeviceMemory memory;
+VkBuffer CreateBufferAndMemory(size_t size, VkDeviceMemory *defviceMemory,
+                               VkBufferUsageFlagBits usage,
+                               VkMemoryPropertyFlagBits memProperties) {
+  VkBuffer buffer;
+  VkDeviceMemory memory;
 
-    VkBufferCreateInfo bufferInfo = {
-        .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
-        .size = size,
-        .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
-        .usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
-    };
+  VkBufferCreateInfo bufferInfo = {
+      .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+      .size = size,
+      .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+      .usage = usage,
+  };
 
-    if (vkCreateBuffer(LogicalDevice, &bufferInfo, NULL, &buffer) != VK_SUCCESS)
-    {
-        printf("Failed to create buffer\n");
-        return VK_NULL_HANDLE;
-    }
+  if (vkCreateBuffer(LogicalDevice, &bufferInfo, NULL, &buffer) != VK_SUCCESS) {
+    printf("Failed to create buffer\n");
+    return VK_NULL_HANDLE;
+  }
 
-    VkMemoryRequirements memoryRequirements;
-    vkGetBufferMemoryRequirements(LogicalDevice, buffer, &memoryRequirements);
+  VkMemoryRequirements memoryRequirements;
+  vkGetBufferMemoryRequirements(LogicalDevice, buffer, &memoryRequirements);
 
-    VkMemoryAllocateInfo memoryAllocInfo = {
-        .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
-        .allocationSize = memoryRequirements.size,
-        .memoryTypeIndex = FindMemoryIndexByType(memoryRequirements.memoryTypeBits,
-                                                 VK_MEMORY_PROPERTY_HOST_CACHED_BIT |
-                                                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT),
-    };
+  VkMemoryAllocateInfo memoryAllocInfo = {
+      .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+      .allocationSize = memoryRequirements.size,
+      .memoryTypeIndex = FindMemoryIndexByType(
+          memoryRequirements.memoryTypeBits, memProperties),
+  };
 
-    if (vkAllocateMemory(LogicalDevice, &memoryAllocInfo, NULL, &memory) != VK_SUCCESS)
-    {
-        printf("Failed to allocate mamory for the buffer\n");
-        vkDestroyBuffer(LogicalDevice, buffer, NULL);
-        return VK_NULL_HANDLE;
-    }
+  if (vkAllocateMemory(LogicalDevice, &memoryAllocInfo, NULL, &memory) !=
+      VK_SUCCESS) {
+    printf("Failed to allocate mamory for the buffer\n");
+    vkDestroyBuffer(LogicalDevice, buffer, NULL);
+    return VK_NULL_HANDLE;
+  }
 
-    if (vkBindBufferMemory(LogicalDevice, buffer, memory, 0) != VK_SUCCESS)
-    {
-        printf("Failed to bind buffer and memmory\n");
+  if (vkBindBufferMemory(LogicalDevice, buffer, memory, 0) != VK_SUCCESS) {
+    printf("Failed to bind buffer and memmory\n");
 
-        vkDestroyBuffer(LogicalDevice, buffer, NULL);
-        return VK_NULL_HANDLE;
-    }
+    vkDestroyBuffer(LogicalDevice, buffer, NULL);
+    return VK_NULL_HANDLE;
+  }
 
-    *defviceMemory = memory;
-    return buffer;
+  *defviceMemory = memory;
+  return buffer;
 }
 
-void CreateBuffers(size_t inputSize, size_t outputSize)
-{
-    InputBuffer = CreateBufferAndMemory(inputSize, &InputBufferMemory);
-    OutputBuffer = CreateBufferAndMemory(outputSize, &OutputBufferMemory);
+void CreateBuffers(size_t inputSize, size_t outputSize) {
+  InputBuffer = CreateBufferAndMemory(inputSize, &InputBufferMemory,
+                                      VK_BUFFER_USAGE_STORAGE_BUFFER_BIT |
+                                      VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                                      VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+  OutputBuffer = CreateBufferAndMemory(outputSize, &OutputBufferMemory,
+                                       VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+                                       VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 
-    VkWriteDescriptorSet writeDescriptorSet = {
-        .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-        .dstSet = DescriptorSet,
-        .dstBinding = 0,
-        .descriptorCount = 2,
-        .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
-        .pBufferInfo = (VkDescriptorBufferInfo[2]){
-            [0].buffer = InputBuffer,
-            [0].offset = 0,
-            [0].range = inputSize,
-            [1].buffer = OutputBuffer,
-            [1].offset = 0,
-            [1].range = outputSize,
-        }
-    };
+  VkWriteDescriptorSet writeDescriptorSet = {
+      .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+      .dstSet = DescriptorSet,
+      .dstBinding = 0,
+      .descriptorCount = 2,
+      .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+      .pBufferInfo = (VkDescriptorBufferInfo[2]){
+          [0].buffer = InputBuffer,
+          [0].offset = 0,
+          [0].range = inputSize,
+          [1].buffer = OutputBuffer,
+          [1].offset = 0,
+          [1].range = outputSize,
+      }};
 
-    vkUpdateDescriptorSets(LogicalDevice, 1, &writeDescriptorSet, 0, NULL);
+  vkUpdateDescriptorSets(LogicalDevice, 1, &writeDescriptorSet, 0, NULL);
 }
 
-void DestroyBuffers(void)
-{
-    vkDestroyBuffer(LogicalDevice, InputBuffer, NULL);
-    vkFreeMemory(LogicalDevice, InputBufferMemory, NULL);
-    vkDestroyBuffer(LogicalDevice, OutputBuffer, NULL);
-    vkFreeMemory(LogicalDevice, OutputBufferMemory, NULL);
+static void CopyData(void *data, size_t size, bool isWrite,
+                      VkDeviceMemory memory) {
+  void *adress;
+  if (vkMapMemory(LogicalDevice, memory, 0, size, 0, &adress) != VK_SUCCESS) {
+    printf("Failed to map buffer memory\n");
+    return;
+  }
+
+  isWrite ? memcpy(adress, data, size) : memcpy(data, adress, size);
+
+  vkUnmapMemory(LogicalDevice, memory);
 }
 
-void CoppyToInputBuffer (void *data, size_t size)
-{
-    void *adress;
-    if (vkMapMemory(LogicalDevice, InputBufferMemory, 0, size, 0, &adress) != VK_SUCCESS)
-    {
-        printf("Failed to map input buffer memory\n");
-        return;
-    }
-    memcpy(adress, data, size);
-
-    vkUnmapMemory(LogicalDevice, InputBufferMemory);
+void CopyToInputBuffer(void *data, size_t size) {
+  CopyData(data, size, true, InputBufferMemory);
 }
 
-void CoppyFromOutputBuffer (void *data, size_t size)
-{
-    void *adress;
-    if (vkMapMemory(LogicalDevice, OutputBufferMemory, 0, size, 0, &adress) != VK_SUCCESS)
-    {
-        printf("Failed to map output buffer memory\n");
-        return;
-    }
-    memcpy(data, adress, size);
+void CopyFromOutputBuffer(void *data, size_t size) {
+  CopyData(data, size, false, OutputBufferMemory);
+}
 
-    vkUnmapMemory(LogicalDevice, OutputBufferMemory);
+void CopyBufferToBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, size_t size)
+{
+
+}
+
+void CopyToLocalBuffer(void *data, size_t size, VkBuffer buffer)
+{
+    VkDeviceMemory tempMemory;
+    VkBuffer tempBuffer = CreateBufferAndMemory(size, &tempMemory,
+    VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+    VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    CopyData(data, size, 1, tempMemory);
+
+    CopyBufferToBuffer(tempBuffer, buffer, size);
+
+    vkDestroyBuffer(LogicalDevice, tempBuffer, NULL);
+    vkFreeMemory(LogicalDevice, tempMemory, NULL);
+}
+
+void DestroyBuffers(void) {
+  vkDestroyBuffer(LogicalDevice, InputBuffer, NULL);
+  vkFreeMemory(LogicalDevice, InputBufferMemory, NULL);
+  vkDestroyBuffer(LogicalDevice, OutputBuffer, NULL);
+  vkFreeMemory(LogicalDevice, OutputBufferMemory, NULL);
 }
